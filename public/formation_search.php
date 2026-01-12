9c02d6fab0aee887b0ebbf60ceb696b9c3d3f6e9
Ad2rien5 <seach.adrien@gmail.com>
2026-01-12 15:51:40 +0100

    première section finit

--- public/formation_search.php
+++ public/formation_search.php
@@ -8,12 +8,20 @@ switch ($type_search) {
         $texte = "Formation";
         break;
     case 'activite':
-        $texte = "Activié";
+        $texte = "Activité";
         break;
     default:
         $texte = "Thématique";
         break;
 }
+
+function is_selected($btn_name, $type_search): string {
+    if ($btn_name == $type_search) {
+        return " btn_selected";
+    }
+    return "";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -38,7 +46,24 @@ switch ($type_search) {
   <main-header></main-header>
 
   <main class="body">
-    <p>Recherche de <?= $texte ?></p>
+    <section class="top_part">
+      <form action="/action_page.php">
+        <h1>Recherche <?= $texte ?></h1>
+
+        <div class="searchbar">
+          <input type="text" placeholder="Search..." name="search"/>
+          <button type="submit" class="search_button">
+            <img src="img/loupe.svg" class="lens" alt="Lancer recherche"/>
+          </button>
+        </div>
+
+        <div class="btn_type">
+          <a type="button" class="type_search<?= is_selected("thematique", $type_search) ?>" href="?type_search=thematique">Thématique</a>
+          <a type="button" class="type_search<?= is_selected("formation", $type_search) ?>" href="?type_search=formation">Formation</a>
+          <a type="button" class="type_search<?= is_selected("activite", $type_search) ?>" href="?type_search=activite">Activité</a>
+        </div>
+      </form>
+    </section>
   </main>
 
   <main-footer></main-footer>
