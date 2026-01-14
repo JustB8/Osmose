9a10a43001ded28a82faf44eab3bc6e1f1dbf90a
Justin BARON <justin.baron2024@campus-eni.fr>
2026-01-14 18:46:33 +0100

    dashboard

--- public/Dashboard.php
+++ public/Dashboard.php
@@ -2,10 +2,6 @@
 session_start();
 require_once 'call_bdd.php';
 
-// --- LOGIQUE DE CONNEXION (Simulation pour le test) ---
-$isLogged = true; 
-$userId = 1; 
-
 $points_user = ['level' => 0];
 $points_entreprise = ['level' => 0];
 
