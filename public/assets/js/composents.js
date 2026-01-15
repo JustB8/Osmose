65c2c61550ceb7967e1f9da8b39408bf392c4d30
Justin Baron <baronjustin500@gmail.com>
2026-01-15 22:37:41 +0100

    Voila

--- public/assets/js/composents.js
+++ public/assets/js/composents.js
@@ -4,10 +4,10 @@ class MyHeader extends HTMLElement {
             this.innerHTML = `
             <nav role="navigation" aria-label="Liens pour skip" >
                 <ul class="skip-links">
-                    <li><a href="#site" class="skip-links__link visually-hidden-focusable">Passer à mon profil</a></li>
-                    <li><a href="#Button_search" class="skip-links__link visually-hidden-focusable">Passer à la déconnexion</a></li>
+                    <li><a href="#bouton-noir-profil" class="skip-links__link visually-hidden-focusable">Passer à mon profil</a></li>
+                    <li><a href="#bouton-blanc-deco" class="skip-links__link visually-hidden-focusable">Passer à la déconnexion</a></li>
                     <li><a href="#main-content" class="skip-links__link visually-hidden-focusable">Passer au contenu principal</a></li>
-                    <li><a href="#main-content" class="skip-links__link visually-hidden-focusable">Passer au bas de page</a></li>
+                    <li><a href="#logo-link-footer" class="skip-links__link visually-hidden-focusable">Passer au bas de page</a></li>
                 </ul>
             </nav>
             <div class="header">
@@ -18,10 +18,10 @@ class MyHeader extends HTMLElement {
                         </a>
                     </div>
                     <nav class="header-auth" aria-label="Navigation principale">
-                        <a href="/profil_util.php" class="bouton-noir">
+                        <a href="/profil_util.php" class="bouton-noir-profil">
                             <span class="button-2">Mon profil</span>
                         </a>
-                        <a href="/logout.php" class="bouton-blanc">
+                        <a href="/logout.php" class="bouton-blanc-deco">
                             <span class="text-wrapper">Déconnexion</span>
                         </a>
                     </nav>
@@ -32,10 +32,10 @@ class MyHeader extends HTMLElement {
             this.innerHTML = `
             <nav role="navigation" aria-label="Liens pour skip" >
                 <ul class="skip-links">
-                    <li><a href="#cree_compte" class="skip-links__link visually-hidden-focusable">Passer à la création de compte</a></li>
-                    <li><a href="#Button_search" class="skip-links__link visually-hidden-focusable">Passer à la connexion</a></li>
+                    <li><a href="#bouton-blanc-crea" class="skip-links__link visually-hidden-focusable">Passer à la création de compte</a></li>
+                    <li><a href="#bouton-noir-con" class="skip-links__link visually-hidden-focusable">Passer à la connexion</a></li>
                     <li><a href="#main-content" class="skip-links__link visually-hidden-focusable">Passer au contenu principal</a></li>
-                    <li><a href="#main-content" class="skip-links__link visually-hidden-focusable">Passer au bas de page</a></li>
+                    <li><a href="#logo-link-footer" class="skip-links__link visually-hidden-focusable">Passer au bas de page</a></li>
                 </ul>
             </nav>
             <div class="header">
@@ -46,10 +46,10 @@ class MyHeader extends HTMLElement {
                         </a>
                     </div>
                     <nav class="header-auth" aria-label="Navigation principale">
-                        <a href="/connexion.php?form=register" class="bouton-blanc">
+                        <a href="/connexion.php?form=register" id="bouton-blanc-crea">
                             <span id="cree_compte" >Créer un compte</span>
                         </a>
-                        <a href="/connexion.php" class="bouton-noir">
+                        <a href="/connexion.php" id="bouton-noir-con">
                             <span id="connexion_compte">Connexion</span>
                         </a>
                     </nav>
@@ -64,119 +64,118 @@ class MyFooter extends HTMLElement {
     connectedCallback() {
         if (window.IS_LOGGED) {
             this.innerHTML = `
-            <footer class="footer">
+            <footer>
                 <a href="/index.php" class="logo-link">
                     <img class="title" src="img/logo.svg" alt="Osmose" />
                 </a>
 
-                <nav class="text-link-list" aria-labelledby="footer-compte">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-compte">
-                            <span class="text-strong-2">Compte</span>
+                <nav class="liste-liens" aria-labelledby="footer-compte">
+                    <div>
+                        <h2 id="footer-compte">
+                            <span class="text-strong">Compte</span>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/profil_util.php" class="list-item">Mon profil</a>
+                    <div class="liste-liens-item">
+                        <a href="/profil_util.php" class="lien">Mon profil</a>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/logout.php" class="list-item-2">Déconnexion</a>
+                    <div class="liste-liens-item">
+                        <a href="/logout.php" class="lien">Déconnexion</a>
                     </div>
                 </nav>
 
-                <nav class="text-link-list" aria-labelledby="footer-leaderboard">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-leaderboard">                            
-                            <p class="text-strong-2">LeaderBoard</p>
+                <nav class="liste-liens" aria-labelledby="footer-leaderboard">
+                    <div>
+                        <h2 id="footer-leaderboard">                            
+                            <p class="text-strong">LeaderBoard</p>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/leaderboard.php" class="list-item-3">Entreprise</a>
+                    <div class="liste-liens-item">
+                        <a href="/leaderboard.php" class="lien">Entreprise</a>
                     </div>
                 </nav>
 
-                <nav class="text-link-list" aria-labelledby="footer-mentions">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-mentions">
-                            <span class="text-strong-2">Mention du site</span>
+                <nav class="liste-liens" aria-labelledby="footer-mentions">
+                    <div>
+                        <h2 id="footer-mentions">
+                            <span class="text-strong">Mention du site</span>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/legal_notice.php" class="list-item-4">Mention légale</a>
+                    <div class="liste-liens-item">
+                        <a href="/legal_notice.php" class="lien">Mention légale</a>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/rgaa.php" class="list-item">Justificatif RGAA</a>
+                    <div class="liste-liens-item">
+                        <a href="/rgaa.php" class="lien">Justificatif RGAA</a>
                     </div>
                 </nav>
 
-                <nav class="text-link-list" aria-labelledby="footer-plan">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-plan">
-                            <span class="text-strong-2">Plan du site</span>
+                <nav class="liste-liens" aria-labelledby="footer-plan">
+                    <div>
+                        <h2 id="footer-plan">
+                            <span class="text-strong">Plan du site</span>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/sitemap.php" class="list-item-5">Plan du site</a>
+                    <div class="liste-liens-item">
+                        <a href="/sitemap.php" class="lien">Plan du site</a>
                     </div>
-                    <div class="text-link-list-item"></div>
+                    <div class="liste-liens-item"></div>
                 </nav>
             </footer>
             `;
         } else {
             this.innerHTML = `
-            <footer class="footer">
-                <a href="/index.php" class="logo-link">
+            <footer>
+                <a href="/index.php" id="logo-link-footer">
                     <img class="title" src="img/logo.svg" alt="Osmose" />
                 </a>
-
-                <nav class="text-link-list" aria-labelledby="footer-compte">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-compte">
-                            <span class="text-strong-2">Compte</span>
+                <nav class="liste-liens" aria-labelledby="footer-compte">
+                    <div>
+                        <h2 id="footer-compte">
+                            <p class="text-strong">Compte</p>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/connexion.php?form=register" class="list-item">Créer un compte</a>
+                    <div class="liste-liens-item">
+                        <a href="/connexion.php?form=register" class="lien">Créer un compte</a>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/connexion.php" class="list-item-2">Se connecter</a>
+                    <div class="liste-liens-item">
+                        <a href="/connexion.php" class="lien">Se connecter</a>
                     </div>
                 </nav>
 
-                <nav class="text-link-list" aria-labelledby="footer-leaderboard">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-leaderboard">
-                            <p class="text-strong-2">LeaderBoard</p>
+                <nav class="liste-liens" aria-labelledby="footer-leaderboard">
+                    <div>
+                        <h2 id="footer-leaderboard">
+                            <p class="text-strong">LeaderBoard</p>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/leaderboard.php" class="list-item-3">Entreprise</a>
+                    <div class="liste-liens-item">
+                        <a href="/leaderboard.php" class="lien">Entreprise</a>
                     </div>
                 </nav>
 
-                <nav class="text-link-list" aria-labelledby="footer-mentions">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-mentions">
-                            <span class="text-strong-2">Mention du site</span>
+                <nav class="liste-liens" aria-labelledby="footer-mentions">
+                    <div>
+                        <h2 id="footer-mentions">
+                            <p class="text-strong">Mention du site</p>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/legal_notice.php" class="list-item-4">Mention légale</a>
+                    <div class="liste-liens-item">
+                        <a href="/legal_notice.php" class="lien">Mention légale</a>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/rgaa.php" class="list-item">Justificatif RGAA</a>
+                    <div class="liste-liens-item">
+                        <a href="/rgaa.php" class="lien">Justificatif RGAA</a>
                     </div>
                 </nav>
 
-                <nav class="text-link-list" aria-labelledby="footer-plan">
-                    <div class="text-strong-wrapper">
-                        <h2 class="text-strong" id="footer-plan">
-                            <span class="text-strong-2">Plan du site</span>
+                <nav class="liste-liens" aria-labelledby="footer-plan">
+                    <div>
+                        <h2 id="footer-plan">
+                            <p class="text-strong">Plan du site</p>
                         </h2>
                     </div>
-                    <div class="text-link-list-item">
-                        <a href="/sitemap.php" class="list-item-5">Plan du site</a>
+                    <div class="liste-liens-item">
+                        <a href="/sitemap.php" class="lien">Plan du site</a>
                     </div>
-                    <div class="text-link-list-item"></div>
+                    <div class="liste-liens-item"></div>
                 </nav>
             </footer>
             `;
